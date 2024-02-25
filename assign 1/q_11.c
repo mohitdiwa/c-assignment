e1c8f22f9c5f091015146678f39796b88de2cea7
mohitdiwa <mohitdiwa19@gmail.com>
2024-02-25 14:41:54 +0530

    v1.o

---
+++ assign 1/q_11.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the time HH:MM");
+    scanf("%d:%d",&a,&b);
+    printf("Time %d Hours and %d Minutes",a,b);
+    return 0; //11 Hour and 25 Minute”
+}
---
+++ assign 1/q_6.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf(" \"Hello, Amit \" " );
+    return 0;
+}
