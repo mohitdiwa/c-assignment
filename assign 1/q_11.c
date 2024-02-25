aadc34fbf9b16fb1f15e58ecd754761b471edb73
mohitdiwa <mohitdiwa19@gmail.com>
2024-02-25 15:40:42 +0530

    v1.0

--- assign 1/q_11.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b;
-    printf("enter the time HH:MM");
-    scanf("%d:%d",&a,&b);
-    printf("Time %d Hours and %d Minutes",a,b);
-    return 0; //11 Hour and 25 Minute”
-}
